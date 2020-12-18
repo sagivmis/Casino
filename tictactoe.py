a7b01135a7b4f9355aecedff3af35c8c641e3666
sagivmis <74502370+sagivmis@users.noreply.github.com>
2020-12-19 01:38:03 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,6 @@
+import random
+
+
 class color:
     GREEN = '\033[92m'
     RED = '\033[91m'
@@ -7,11 +10,12 @@ class color:
     # BG Colors
     BG_RED = '\033[41m'
     BG_WHITE = '\033[47m'
-import os
+
 
 def clear_screen():
-    for i in range(100):
+    for i in range(250):
         print()
+
 
 def player_input():
     choice = False  # not yet to choose correctly
@@ -33,20 +37,17 @@ def place_holder():
 
 def win_check(board, mark):
     for i in range(1, 10, 3):
-        if (board[i] == board[i + 1] == board[i + 2] == mark):
+        if board[i] == board[i + 1] == board[i + 2] == mark:
             return True
     for i in [1, 2, 3]:
-        if (board[i] == board[i + 3] == board[i + 6] == mark):
+        if board[i] == board[i + 3] == board[i + 6] == mark:
             return True
-    if (board[1] == board[5] == board[9] == mark):
+    if board[1] == board[5] == board[9] == mark:
         return True
-    if (board[3] == board[5] == board[7] == mark):
+    if board[3] == board[5] == board[7] == mark:
         return True
 
     return False
-
-
-import random
 
 
 def choose_first():
@@ -59,7 +60,7 @@ def space_check(board, position):
 
 def full_board_check(board):
     for i in range(1, 10):
-        if (board[i] != 'X' or board[i] != 'O'):
+        if board[i] == ' ':
             return False
     return True
 
@@ -69,22 +70,25 @@ def player_choice(board):
     if space_check(board, pos):
         return pos
     else:
-        while space_check(board,pos) != True:
+        while not space_check(board, pos):
             pos = int(input("What position you want to place?"))
         if space_check(board, pos):
             return pos
 
+
 def replay():
-    again = input("Another game?")
-    while again != "Yes" and again !="No":
+    again = input("Another game? y or n")
+    while again != "y" and again != "n":
         again = input("Another game?")
-    if again == "Yes":
+    if again == "y":
         return True
     else:
         return False
 
 
-def display_board(board, switch): #switch = 1 for win 0 for in-game
+def display_board(board, switch, player=None):  # switch = 1 for win 0 for in-game
+    if player:
+        print(f"Current marker's turn: {player}")
     for i in range(1, 4):
         print(color.BOLD + color.GREEN + f"{board[i]}" + color.END, end=" | ")
     print()
@@ -98,10 +102,11 @@ def display_board(board, switch): #switch = 1 for win 0 for in-game
     print()
     print("___________")
 
-def game_on():
-    score = [0,0]
+
+def run_tictactoe_game():
+    score = [0, 0]
     tie = 0
-    print(color.BOLD+"Welcome to TIC TAC TOE"+color.END)
+    print(color.BOLD + "Welcome to TIC TAC TOE" + color.END)
     while True:
         print(f"The SCORE is:\n\tPLAYER 1: {score[0]}\n\tPLAYER 2: {score[1]}\n\t\tNumber of ties: {tie}")
         game_board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
@@ -115,7 +120,8 @@ def game_on():
         if player1 == 'O':
             player2 = 'X'
 
-        turn=choose_first()
+
+        turn = choose_first()
         print(f"Player {turn} will go first.")
         play_game = input("Play? y or n")
         while play_game != 'y' and play_game != 'n':
@@ -128,45 +134,45 @@ def game_on():
 
         while gamer_on:
             if turn == 1:
-                #player 1 turn
-                display_board(game_board, 0)
+                # player 1 turn
+                display_board(game_board, 0, player1)
                 position = player_choice(game_board)
-                place_marker(game_board,player1,position)
+                place_marker(game_board, player1, position)
                 if win_check(game_board, player1):
                     clear_screen()
-                    score[0]+=1
-                    display_board(game_board, 0)
+                    score[0] += 1
+                    display_board(game_board, 0, player1)
                     print("Player 1 has won !")
-                    gamer_on=False
+                    gamer_on = False
                 else:
                     if full_board_check(game_board):
-                        print("Tied")
-                        tie+=1
+                        tie += 1
                         clear_screen()
+                        print("Tied")
                         display_board(game_board, 0)
-                        gamer_on=False
+                        gamer_on = False
                     else:
                         turn = 2
                         clear_screen()
                         print(f"Its Player {turn}'s turn")
             else:
-                #player 2 turn
-                display_board(game_board, 0)
+                # player 2 turn
+                display_board(game_board, 0, player2)
                 position = player_choice(game_board)
-                place_marker(game_board,player2,position)
+                place_marker(game_board, player2, position)
                 if win_check(game_board, player2):
                     clear_screen()
-                    score[1]+=1
-                    display_board(game_board, 0)
+                    score[1] += 1
+                    display_board(game_board, 0, player2)
                     print("Player 2 has won !")
                     gamer_on = False
                 else:
                     if full_board_check(game_board):
-                        print("Tied")
-                        tie+=1
+                        tie += 1
                         clear_screen()
+                        print(f"Tied")
                         display_board(game_board, 0)
-                        gamer_on=False
+                        gamer_on = False
                     else:
                         turn = 1
                         clear_screen()
@@ -174,4 +180,3 @@ def game_on():
         if not replay():
             print(f"The SCORE is:\n\tPLAYER 1: {score[0]}\n\tPLAYER 2: {score[1]}\n\t\tNumber of ties: {tie}")
             break
-game_on()
