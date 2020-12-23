a80027448aef080591d009499a7c66e717ed5086
sagivmis <74502370+sagivmis@users.noreply.github.com>
2020-12-24 00:51:31 +0200

    Update casino.py

--- casino.py
+++ casino.py
@@ -1,7 +1,9 @@
 import random
-
+import cfg
 from tictactoe import run_tictactoe_game
-
+from war import run_war
+from black_jack import play_bj
+from roulette import roulette
 
 class color:
     BLUE = '\033[94m'
@@ -16,6 +18,32 @@ class color:
     BG_RED = '\033[41m'
     BG_GREEN = '\033[42m'
     BG_WHITE = '\033[47m'
+
+
+class Deck:
+
+    def __init__(self):
+        self.deck = []
+
+        for suit in cfg.suits:
+            for rank in cfg.ranks:
+                self.deck.append(Card(cfg.values[rank], suit, cfg.colordict[suit]))
+        random.shuffle(self.deck)
+
+    def __str__(self):
+        i = 0
+        string = ""
+        for _ in cfg.suits:
+            for _ in cfg.ranks:
+                string += f"{self.deck[i]}\t"
+                i += 1
+        return string
+
+    def deck_len(self):
+        count = 0
+        for _ in self.deck:
+            count += 1
+        return count
 
 
 class Card:
@@ -68,9 +96,16 @@ class Player:
         self.balance = balance
         self.cards = []
         self.totalcards = 0
+        self.hand = Hand()
+        self.bet = 0
 
     def __str__(self):
         return f"Player's name:{self.name}\nBalance:{self.balance}"
+
+    def draw_card_from_deck(self, deck):
+        x = deck.pop()
+        self.cards.append(x)
+        return x
 
     def cards_len(self):
         count = 0
@@ -94,11 +129,11 @@ class Player:
         for i in range(self.cards_len()):
             self.cards.pop()
 
-    def draw_card(self):
+    def draw_random_card(self):
         x = random.randint(1, 13)
         shape = random.randint(1, 4)
-        colordict = {'Spade': 'Black', 'Club': 'Black', 'Heart': 'Red', 'Diamond': 'Red'}
-        newcard = Card(x, shape_shift(shape), colordict[shape_shift(shape)])
+        # cfg.colordict = {'Spade': 'Black', 'Club': 'Black', 'Heart': 'Red', 'Diamond': 'Red'}
+        newcard = Card(x, shape_shift_from_number(shape), cfg.colordict[shape_shift_from_number(shape)])
         if newcard.number >= 11:
             newcard.number = 10
         self.cards.append(newcard)
@@ -117,16 +152,59 @@ class Player:
                 self.balance -= bet_amount
                 return bet_amount
 
+    def lose(self, bet_amount):
+        print(
+            f"Im sorry :(\nYou lost {bet_amount} $, better luck next time!\n" + color.RED + "Your current balance:" + color.END,
+            self.get_balance())
 
-def shape_shift(shape):
-    """
-    :param shape:
-    :type shape:
-    :return:
-    :rtype:
-    1 for Hearts , 2 Spades, 3 Diamonds, 4 Clubs
-    """
+    def win(self, chips_won):
+        self.add_to_balance(chips_won)
+        print(color.BOLD + color.GREEN + "Congratulations!" + color.END + f"\nYou have won {chips_won} $!!\n"
+                                                                          f"Your balance is now {self.get_balance()}")
 
+    def tie(self, bet):
+        self.add_to_balance(bet)
+        print(color.BOLD + color.YELLOW + "Tied !" + color.END)
+
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+        self.total_cards = 0
+
+    def add_card(self, card):
+        if card.number >= 11:
+            card.number = 10
+        self.cards.append(card)
+        self.total_cards = self.cards_len()
+
+    def cards_len(self):
+        count = 0
+        for i in self.cards:
+            count += 1
+        return count
+
+    def adjust_for_ace(self):
+        if self.aces > 0:
+            if self.value > 21:
+                self.value -= 10
+
+    def draw_card_from_deck(self, played_deck):
+        temp_card = played_deck.deck.pop()
+        if played_deck.deck_len() > 0:
+            self.cards.append(temp_card)
+        else:
+            print("No more cards in deck")
+        return temp_card
+
+    def reset_hand_card_array(self):
+        for i in range(self.cards_len()):
+            self.cards.pop()
+
+
+def shape_shift_from_number(shape):
     shapes = {
         1: 'Heart',
         2: 'Spade',
@@ -139,10 +217,10 @@ def shape_shift(shape):
 
 def build_deck_n_shuffle():  # working
     deck = []
-    colordict = {'Spade': 'Black', 'Club': 'Black', 'Heart': 'Red', 'Diamond': 'Red'}
+    #    colordict = {'Spade': 'Black', 'Club': 'Black', 'Heart': 'Red', 'Diamond': 'Red'}
     for shape in range(1, 5):
         for card in range(1, 14):
-            newcard = Card(card, shape_shift(shape), colordict[shape_shift(shape)])
+            newcard = Card(card, shape_shift_from_number(shape), cfg.colordict[shape_shift_from_number(shape)])
             deck.append(newcard)
             print(newcard)
     random.shuffle(deck)
@@ -150,9 +228,9 @@ def build_deck_n_shuffle():  # working
     return deck
 
 
-def sum_list(l):
+def sum_list(lst):
     total = 0
-    for val in l:
+    for val in lst:
         total += val
     return total
 
@@ -160,8 +238,14 @@ def sum_list(l):
 def check_war(player, dealer):
     if player > dealer:
         return True
-    else:
+    elif player < dealer:
         return False
+    # else:
+    #     for i in range(3):
+    #         player.draw_card_from_deck(player.hand)
+    #         dealer.draw_card_from_deck(dealer.hand)
+    #     at_war=False
+    #     check_war(player,dealer)
 
 
 def sum_cards(cards):
@@ -171,17 +255,10 @@ def sum_cards(cards):
     return sum
 
 
-def count_cards(cards):
-    count = 0
-    for _ in cards:
-        count += 1
-    return count
-
-
 def draw_dealer(dealer, cards, sumofcards):
     print("Generating cards for dealer.......... (must hit at-least 17)")
     while sumofcards < 17:
-        dealer.draw_card()
+        dealer.draw_random_card()
         sumofcards = sum_cards(dealer.cards)
     return
 
@@ -193,23 +270,6 @@ def check_bet(balance, bet):  # checks if the bet is legit (=not higher than bal
     else:
         return bet
     return 0
-
-
-def lose(player, betAmount):
-    print(
-        f"Im sorry :(\nYou lost {betAmount}, better luck next time!\n" + color.RED + "Your current balance:" + color.END,
-        player.get_balance())
-
-
-def win(player, chips_won):
-    player.add_to_balance(chips_won)
-    print(color.BOLD + color.GREEN + "Congratulations!" + color.END + f"\nYou have won {chips_won}.\n"
-                                                                      f"And your balance is now {player.get_balance()}")
-
-
-def tie(player, bet):
-    player.add_to_balance(bet)
-    print(color.BOLD + color.YELLOW + "Tied !" + color.END)
 
 
 def game_pick():  # inputs what game player wants to play
@@ -248,143 +308,33 @@ def show_card(card):
     return card.shape
 
 
-card = 0
-oppcard = 0
-name = input("Input your name")
-newplayer = Player(name, 100)
-opponent = Player("OP", 9999999)
-game_picked_by_player = game_pick()
-
 if __name__ == '__main__':
+    name = input("Input your name")
+    cfg.new_player.name = name
+
+    game_picked_by_player = game_pick()
+    playing_deck = Deck()
+
     while game_picked_by_player != 9:
-        if game_picked_by_player == 18:
-            deck = build_deck_n_shuffle()
-            for i in range(len(deck)):
-                show_card(deck[i])
-                print("*" * 15)
-            # print(deck[i], end=" , ")
-            game_picked_by_player = game_pick()
+
         if game_picked_by_player == 4:
             run_tictactoe_game()
             game_picked_by_player = game_pick()
-        if game_picked_by_player == 1:
-            print("WELCOME TO WAR \nstart game? \n\t1=Yes \n\t2=No")
-            startGame = int(input())
-            while startGame == 1:
-                bet = newplayer.new_bet()
-                card = Card(random.randint(1, 13), shape_shift(random.randint(1, 4)))
-                print(f"Your card: ", color.BOLD + show_card(card) + color.END)
-                oppcard = Card(random.randint(1, 13), shape_shift(random.randint(1, 4)))
-                print(f"Opponent's card: ", color.BOLD + show_card(oppcard) + color.END)
-                if card.number < oppcard.number:
-                    lose(newplayer, bet)
-                    print("\nanother game?\n1=Yes\n2=No")
-                    startGame = int(input())
-                elif card.number == oppcard.number:
-                    tie(newplayer, bet)
-                    print("Another game?\n\t1=Yes\n\t2=No")
-                    startGame = int(input())
-                else:
-                    win(newplayer, bet * 2)
-                    print("\nanother game?\n1=Yes\n2=No")
-                    startGame = int(input())
-            game_picked_by_player = game_pick()
-        if game_picked_by_player == 2:
-            print(color.BOLD+"WELCOME TO ROULETTE"+color.END)
-            randInt = random.randint(1, 36)
-            print("Pick your choice \n\t1.(Numbers 1-36)\n\t2.(Colors)\n\n\t9.Exit")
-            choice = int(input())
-            if choice == 1:
-                betAmount = [0]
-                playerBet = [0]
-                print("Enter the number to bet on (1-36):")
-                playerBet[0] = int(input())
-                if playerBet[0] == 99:  # for check. 99 for an array that will win.
-                    playerBet = list(range(37))
-                betAmount[0] = newplayer.new_bet()
-                print("Another bet? \n\t", color.BOLD + color.GREEN + "1-Yes" + color.END,
-                      color.BOLD + color.RED + "\n\t2-No" + color.END)
-                anotherbet = int(input())
-                while anotherbet == 1:
-                    print("On what number? 1-36")
-                    x = int(input())
-                    playerBet.append(x)
-                    x = newplayer.new_bet()
-                    betAmount.append(x)
-                    print("Another bet? \n\t", color.BOLD + color.GREEN + "1-Yes" + color.END,
-                          color.BOLD + color.RED + "\n\t2-No" + color.END)
-                    anotherbet = int(input())
-                index_solution = 0
-                betAmountSum = sum_list(betAmount)
-                isItIn = randInt in playerBet
-                if isItIn:
-                    index_solution = playerBet.index(randInt)
-                    if playerBet == list(range(37)):
-                        betAmount = list([10] * 37)  # inputs 10 as a bet for all 'tries' - also, a check
-                    print(color.BOLD + color.GREEN + f"the number is....{randInt}" + color.END)
-                    win(newplayer, (betAmount[index_solution]) * 36)
-                else:
-                    print(color.BOLD + color.RED + f"the number is....{randInt}\n" + color.END)
-                    lose(newplayer, betAmountSum)
 
-            if choice == 2:
-                print("What color would you like to bet on?\n\t1-Black\n\t2-Red")
-                playerBet = int(input())
-                betAmount = newplayer.new_bet()
-                color_result = random.randint(1, 2)
-                colordict= {
-                    1: 'Black',
-                    2: 'Red'
-                }
-                if playerBet == color_result:
-                    win(newplayer, betAmount * 2)
-                else:
-                    lose(newplayer, betAmount)
-            if choice == 9:
-                game_picked_by_player = game_pick()
-                continue
+        if game_picked_by_player == 1:
+            run_war()
+            game_picked_by_player = game_pick()
+
+        if game_picked_by_player == 2:
+            roulette()
+            game_picked_by_player = game_pick()
+
+
         if game_picked_by_player == 3:
-            print(color.BOLD+"WELCOME TO BLACK JACK"+color.END)
-            newplayer.draw_card()
-            newplayer.draw_card()
-            betAmount = newplayer.new_bet()
-            cardsSum = sum_cards(newplayer.cards)  # player's total
-            print(f"Your cards are: {newplayer.get_card(0)}, {newplayer.get_card(1)} \nSum of cards:\t {cardsSum}\n")
-            opponent.draw_card()
-            opponent.draw_card()
-            oppSum = sum_cards(opponent.cards)  # dealer's total
-            print(f"Dealer cards are:{opponent.get_card(0)}, {opponent.get_card(1)} \n"
-                  f"And the sum of the dealer's cards are:{oppSum} \n\nAnother card?", color.BOLD + color.GREEN +
-                  "\n\t1-Yes" + color.END, color.BOLD + color.RED + "\n\t2-No" + color.END)
-            if oppSum < 17:
-                draw_dealer(opponent, opponent.cards, oppSum)
-                oppSum = sum_cards(opponent.cards)
-                print("Dealer has drawn more cards. New total sum of dealer's cards is: ", oppSum)
-            anotherCard = int(input())
-            while anotherCard == 1 and cardsSum < 22:
-                newplayer.draw_card()
-                cardsSum = sum_cards(newplayer.cards)
-                print(f"Next cards is: {newplayer.cards[len(newplayer.cards) - 1]} and the total sum is {cardsSum}\n"
-                      f"Another card?\n\t", color.BOLD + color.GREEN + "1-Yes" + color.END, color.BOLD + color.RED +
-                      "\n\t2-No" + color.END)
-                anotherCard = int(input())
-            if cardsSum > 21:
-                print("BURNT!")
-            win_check = check_BJ(cardsSum, oppSum)
-            if win_check:
-                if newplayer.cards_len() == 2 and cardsSum == 21:
-                    betAmount = int(betAmount * 1.5)
-                    print(color.BG_WHITE + color.RED + "BJ!!!" + color.END)
-                win(newplayer, betAmount * 2)
-            else:
-                lose(newplayer, betAmount)
-            print("Another game?", color.BOLD + color.GREEN + "\n\t1-Yes" + color.END,
-                  color.BOLD + color.RED + "\n\t2-No" + color.END)
-            anotherBJ = int(input())
-            newplayer.reset_card_array()
-            opponent.reset_card_array()
-            if anotherBJ == 2:
-                game_picked_by_player = game_pick()
+            play_bj()
+            game_picked_by_player = game_pick()
+
     if game_picked_by_player == 9:
-        print(color.BOLD + color.RED + f"Your end balance: {newplayer.get_balance()}\nGoodbye {newplayer.name}."
-              + color.END)
+        print(
+            color.BOLD + color.RED + f"Your end balance: {cfg.new_player.get_balance()}\nGoodbye {cfg.new_player.name}."
+            + color.END)
